d46b95453cb08a74073b6e2068651a3d7c5663ac
example <example@example.com>
2025-09-17 18:14:03 -0400

    Refactor X11 configuration to enable video drivers and set keymap

--- conf.nix
+++ conf.nix
@@ -35,17 +35,23 @@
 
   # Enable the X11 windowing system.
   # You can disable this if you're only using the Wayland session.
-  services.xserver.enable = true;
+  services.xserver = {
+    enable = true;
+    videoDrivers = [
+      "intel"
+      "nvidia"
+    ];
+    
+    # Configure keymap in X11
+    xkb = {
+        layout = "us";
+        variant = "";
+    };
+  };
 
   # Enable the KDE Plasma Desktop Environment.
   services.displayManager.sddm.enable = true;
   services.desktopManager.plasma6.enable = true;
-
-  # Configure keymap in X11
-  services.xserver.xkb = {
-    layout = "us";
-    variant = "";
-  };
 
   # Enable CUPS to print documents.
   services.printing.enable = true;
